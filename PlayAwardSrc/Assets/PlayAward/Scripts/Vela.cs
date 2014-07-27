648fe76b5b0d86c18d4ee12d794f9d5eb408f2da
exAntares <luis_andres81@hotmail.com>
2014-07-27 15:00:59 +0200

    Vela Usable V0.01

---
+++ PlayAwardSrc/Assets/PlayAward/Scripts/Vela.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vela : MonoBehaviour
+{
+	public int NumVela = 0;
+	void OnUse()
+	{
+		print("Hola! soy la vela #" + NumVela);
+	}
+}
