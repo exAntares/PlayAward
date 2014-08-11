282d91d662ef4de8691ed5ed66455501cc3c68c1
exAntares <luis_andres81@hotmail.com>
2014-08-12 00:36:35 +0200

    Niveles aditivos, en todos los mapas, excepto Main porque no se quien lo tiene xD

--- PlayAwardSrc/Assets/PlayAward/Scripts/MultiUse/GameMode.cs
+++ PlayAwardSrc/Assets/PlayAward/Scripts/MultiUse/GameMode.cs
@@ -2,27 +2,79 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameMode : MonoBehaviour {
+public class GameMode : Singleton<GameMode>
+{
+    public DefaultObjects _DefaultObjects;
+    public List<string> ScenesToLoad = new List<string>();
 
-	public NeededObjects NecessaryObjects;
+    private List<string> ScenesLoaded = new List<string>();
+    private string thisLevel;
 
-	// Use this for initialization
-	void awake () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private GameMode() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    // Use this for initialization
+    void Start()
+    {
+        thisLevel = Application.loadedLevelName;
+        print("Level Loaded " + thisLevel);
+
+        Invoke("LoadAllLevels", 1.0f);
+    }
+
+    void LoadAllLevels()
+    {
+        foreach (string scene in ScenesToLoad)
+        {
+            if (scene != thisLevel && !ScenesLoaded.Contains(scene))
+            {
+                Application.LoadLevelAdditive(scene);
+                ScenesLoaded.Add(scene);
+            }
+        }
+
+        Invoke("PostLoadLevels", 0.1f);
+    }
+
+    void PostLoadLevels()
+    {
+        _DefaultObjects.Init();
+    }
+
 }
 
 [System.Serializable]
-public class NeededObjects
+public class DefaultObjects
 {
-	public GameObject Player;
-	public GameObject StartPoint;
-	public GameObject Fader;
+    public GameObject DefaultStartPoint;
+	public GameObject DefaultPlayer;
+	public GameObject DefaultFader;
 
-	public List<GameObject> Others;
+    public void Init()
+    {
+        GameObject _DefaultObjects = GameObject.Find("DefaultObjects") as GameObject;
+        if (!_DefaultObjects)
+        {
+            _DefaultObjects = new GameObject("DefaultObjects");
+        }
+
+        if (!GameObject.FindGameObjectWithTag(DefaultStartPoint.tag))
+        {
+            DefaultStartPoint = GameObject.Instantiate(DefaultStartPoint) as GameObject;
+            DefaultStartPoint.name = "DefaultStartPoint";
+            DefaultStartPoint.transform.parent = _DefaultObjects.transform;
+        }
+
+        if (!GameObject.FindGameObjectWithTag(DefaultPlayer.tag))
+        {
+            DefaultPlayer = GameObject.Instantiate(DefaultPlayer, DefaultStartPoint.transform.position, DefaultStartPoint.transform.rotation) as GameObject;
+            DefaultPlayer.name = "DefaultPlayer";
+        }
+
+        if (!GameObject.FindGameObjectWithTag(DefaultFader.tag))
+        {
+            DefaultFader = GameObject.Instantiate(DefaultFader) as GameObject;
+            DefaultFader.name = "DefaultFader";
+            DefaultFader.transform.parent = _DefaultObjects.transform;
+        }
+    }
 }
