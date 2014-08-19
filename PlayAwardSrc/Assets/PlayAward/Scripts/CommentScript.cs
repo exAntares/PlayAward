88b6af26734073e08a183983e886bded73a3e4a4
exAntares <luis_andres81@hotmail.com>
2014-08-19 23:11:05 +0200

    Mejoras en el comentario prefab

---
+++ PlayAwardSrc/Assets/PlayAward/Scripts/CommentScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommentScript : MonoBehaviour
+{
+
+    [Multiline]
+    public string Comment;
+
+	// Use this for initialization
+	void Start ()
+    {
+        UpdateText();
+	}
+
+    [ContextMenu("UpdateText")]
+    void UpdateText()
+    {
+        TextMesh textMesh = GetComponent<TextMesh>();
+        if (textMesh)
+        {
+            textMesh.text = Comment;
+        }
+
+    }
+}
