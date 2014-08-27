b8412638bcf40279082afd10bbd172c3fc14dfef
exAntares <luis_andres81@hotmail.com>
2014-08-27 21:22:20 +0200

    Test Loading

---
+++ PlayAwardSrc/Assets/PlayAward/Scripts/_VerySpecificScripts_/InitLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitLoad : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("InitGame", 5.0f);
+	}
+
+    void InitGame()
+    {
+        Application.LoadLevel("Main_Hall_Design");
+    }
+}
