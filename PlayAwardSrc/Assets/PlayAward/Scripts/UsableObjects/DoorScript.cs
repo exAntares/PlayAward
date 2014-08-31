d9414502c88e6484c23a1ff819cd4526411c06b1
exAntares <luis_andres81@hotmail.com>
2014-08-31 15:50:09 +0200

    Sonidos

--- PlayAwardSrc/Assets/PlayAward/Scripts/UsableObjects/DoorScript.cs
+++ PlayAwardSrc/Assets/PlayAward/Scripts/UsableObjects/DoorScript.cs
@@ -9,8 +9,9 @@ public class DoorScript : UsableObject
 		OpenIn,
         OpenOut
 	}
-
+    public DoorSounds Sounds = null;
     public bool Locked = false;
+
 	public DoorState State = DoorState.Closed;
 	[Range(0.0f,1.0f)]
 	public float DoorPosition = 0.0f;
@@ -53,8 +54,13 @@ public class DoorScript : UsableObject
                 DoorPosition -= Speed * Time.deltaTime;
             }
 
-            if(Mathf.Abs(DoorPosition) <= 0.1f)
+            if (Mathf.Abs(DoorPosition) <= 0.1f && DoorPosition != 0.0f)
             {
+                if (Sounds != null && Sounds.ClosedSound)
+                {
+                    AudioSource.PlayClipAtPoint(Sounds.ClosedSound, transform.position);
+                }
+
                 DoorPosition = 0.0f;
             }
 
@@ -69,19 +75,28 @@ public class DoorScript : UsableObject
 
     override public void OnUse(GameObject User)
 	{
-        if (Locked) return;
+        if (Locked)
+        {
+            if (Sounds != null && Sounds.LockedSound)
+            {
+                AudioSource.PlayClipAtPoint(Sounds.LockedSound, transform.position);
+            }
+            return;
+        }
 
 		switch(State)
 		{
         case DoorState.OpenOut:
             State = DoorState.Closed;
             break;
-		case DoorState.OpenIn:
+		case DoorState.OpenIn:            
 			State = DoorState.Closed;
 			break;
         case DoorState.Closed:
             {
                 print(Quaternion.Dot(User.transform.rotation, transform.parent.transform.rotation));
+                
+                PlayOpenSound();
 
                 if (Mathf.Abs(Quaternion.Dot(User.transform.rotation, transform.parent.transform.rotation)) <= 0.7f)
                 {
@@ -96,9 +111,25 @@ public class DoorScript : UsableObject
 		}
 	}
 
+    void PlayOpenSound()
+    {
+        if (Sounds != null && Sounds.OpenSound)
+        {
+            AudioSource.PlayClipAtPoint(Sounds.OpenSound, transform.position);
+        }
+    }
+
 	void EventOnTriggerExit(GameObject Trigger)
 	{
 		State = DoorState.Closed;
 	}
 
+}
+
+[System.Serializable]
+public class DoorSounds
+{
+    public AudioClip LockedSound = null;
+    public AudioClip OpenSound = null;
+    public AudioClip ClosedSound = null;
 }
