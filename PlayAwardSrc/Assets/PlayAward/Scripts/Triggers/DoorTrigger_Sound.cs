f08c60bf21fa43dbb41e36839c5dd4a6c105f748
exAntares <luis_andres81@hotmail.com>
2014-09-02 21:35:38 +0200

    Trigger para Ori

---
+++ PlayAwardSrc/Assets/PlayAward/Scripts/Triggers/DoorTrigger_Sound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoorTrigger_Sound : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    public List<Animator> PuertasAnimar;
+    public string AnimationName = "NOMBRE_DE_LA_ANIMACION";
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (SoundToPlay)
+        {
+            AudioSource.PlayClipAtPoint(SoundToPlay, transform.position);
+        }
+        
+        DoorsPlayAnimation(AnimationName);
+    }
+
+    void DoorsPlayAnimation(string AnimationName)
+    {
+        foreach (Animator anim in PuertasAnimar)
+        {
+            anim.Play(AnimationName);
+        }
+    }
+}
