621469338a292a956cbe7e17f41eef88b79149c2
exAntares <luis_andres81@hotmail.com>
2014-09-06 17:21:43 +0200

    Events

---
+++ PlayAwardSrc/Assets/PlayCore/Actions/Conditions/EventCondition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using Actions;
+
+[System.Serializable]
+public class EventCondition : CCondition
+{
+    public EventsTypes.EEventsTypes Event;
+    public EventsHandler EventOwner;
+
+    public void Awake()
+    {
+        if (EventOwner)
+        {
+            EventOwner.RegisterFunctionToEvent(Execute, Event.ToString());
+        }
+    }
+
+    public void Execute()
+    {
+        foreach (CAction action in _ca.Actions)
+        {
+            action.Play();
+        }
+    }
+}
